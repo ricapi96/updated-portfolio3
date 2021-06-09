ab59a01a134bf4bce7eeec726f67c2c0ffc52a52
Erica Roquemore <enroquemore@gmail.com>
2021-06-08 22:51:10 -0400

    added button functionality

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 console.log("Hello there!");
+
+const homeBtn = document.getElementById('home-btn');
+
+homeBtn.addEventListener('click', function () {
+    event.preventDefault();
+    window.location.href = "/index.html"
+});
